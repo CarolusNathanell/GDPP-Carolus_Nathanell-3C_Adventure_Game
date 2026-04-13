9336a2818b143142bf31bc5514052990c8691b39
Carolus Nathanell <c.nathanell05@gmail.com>
2026-04-13 10:30:16 +0700

    feat: Setup & Third Person Camera

--- GDPP-Martikulasi-P_3C/Assets/Game/Scripts/Player/PlayerMovement.cs
+++ GDPP-Martikulasi-P_3C/Assets/Game/Scripts/Player/PlayerMovement.cs
@@ -33,6 +33,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Vector3 _climbOffset;
     [SerializeField] private float _climbSpeed;
 
+	[SerializeField] private Transform _cameraTransform;
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -47,6 +49,7 @@ public class PlayerMovement : MonoBehaviour
         _input.OnJumpInput += Jump;
         _input.OnClimbInput += StartClimb;
         _input.OnCancelClimb += CancelClimb;
+        HideAndLockCursor();
     }
 
     private void Update()
@@ -75,7 +78,7 @@ public class PlayerMovement : MonoBehaviour
         {
             if (axisDirection.magnitude >= 0.1)
             {
-                float rotationAngle = Mathf.Atan2(axisDirection.x, axisDirection.y) * Mathf.Rad2Deg;
+                float rotationAngle = Mathf.Atan2(axisDirection.x, axisDirection.y) * Mathf.Rad2Deg + _cameraTransform.eulerAngles.y;
                 float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationAngle, ref _rotationSmoothVelocity, _rotationSmoothTime);
                 transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
                 movementDirection = Quaternion.Euler(0f, rotationAngle, 0f) * Vector3.forward;
@@ -164,5 +167,11 @@ public class PlayerMovement : MonoBehaviour
             _rigidbody.useGravity = true;
             transform.position -= transform.forward * 1f;
         }
+    }
+    
+    private void HideAndLockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 }
